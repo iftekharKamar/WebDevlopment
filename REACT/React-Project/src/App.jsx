19404caa8bfa0e5f0c2e2cc0ae53a36861a2f271
Iftekhar Kamar <iftekharkamar@gmail.com>
2024-07-09 23:03:35 +0530

    working on react

--- REACT/React-Project/src/App.jsx
+++ REACT/React-Project/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Button from './Button.jsx'
 import ProductTab from "./ProductTab.jsx"
+import Form from './form.jsx'
 
 function App() {
   return  <>
-  <ProductTab/>
+  {/* <Button/> */}
+  <Form/>
   </>
 }
 
