f6be82129e3d289feb925c680f0f06e4c309ee2b
Iftekhar Kamar <iftekharkamar@gmail.com>
2024-07-13 00:13:29 +0530

    Working form

---
+++ REACT/stateForm-Project/src/form.jsx
@@ -0,0 +1,52 @@
+
+import React, { useState } from 'react';
+
+const Form = () => {
+    const [formData, setFormData] = useState({
+        fullName: "",
+        userName: ""
+    });
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formData);
+        setFormData({
+            fullName: "",
+            userName: ""
+        });
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Full Name:</label>
+                <input
+                    type="text"
+                    name="fullName"
+                    value={formData.fullName}
+                    onChange={handleChange}
+                />
+            </div>
+            <div>
+                <label>User Name:</label>
+                <input
+                    type="text"
+                    name="userName"
+                    value={formData.userName}
+                    onChange={handleChange}
+                />
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    );
+};
+
+export default Form;
